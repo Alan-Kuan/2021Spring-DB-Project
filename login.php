e38d9553a3a59b0a99ef410951125b004c3206bb
Alan-Kuan <kuanhaoan@gmail.com>
2021-04-16 00:26:33 +0800

    Add: login functionality.

--- login.php
+++ login.php
@@ -2,56 +2,56 @@
     session_start();
     $_SESSION['Authenticated'] = false;
 
-    $dbhostname = getenv('DB_HOST');
+    $dbhostname = getenv('MYSQL_HOST');
     $dbport = '3306';
     $dbname = getenv('MYSQL_DATABASE');
     $dbusername = getenv('MYSQL_USER');
     $dbpassword = getenv('MYSQL_PASSWORD');
 
-    if(isset($_POST['username'])) {
+    if(isset($_POST['username']) && isset($_POST['password'])) {
 
         $username = $_POST['username'];
+        $hashed_password = hash('sha256', $_POST['password']);
 
         try {
 
             $conn = new PDO("mysql:host=$dbhostname;port=$dbport;dbname=$dbname", $dbusername, $dbpassword);
             # set the PDO error mode to exception
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $stmt=$conn->prepare("SELECT username FROM users WHERE username=:username");
+            $stmt=$conn->prepare("SELECT password FROM users WHERE username = :username");
             $stmt->execute(array('username' => $username));
 
-            if($stmt->rowCount() == 1) {
-                $row = $stmt->fetch();
-                $hashvalue = hash('sha256', $username);
+            if($stmt->rowCount() == 1 && $stmt->fetch()[0] === $hashed_password) {
                 $_SESSION['Authenticated'] = true;
-                $_SESSION['Username'] = $row[0];
-                header("Location: list.php?page=1");
+                $_SESSION['Username'] = $username;
+                header("Location: home.php");
                 exit();
             } else {
                 session_unset();
                 session_destroy();
-                echo <<<EOT
-                    <!DOCTYPE html>
-                    <html>
-                        <body>
-                            <script>alert("Login failed.");window.location.replace("index.php");</script>
-                        </body>
-                    </html>
-EOT;
+                sendPopupAndGoto('登入失敗', 'index.php');
+                exit();
             }
 
-        } catch(PDOException$e) {
-            $msg = $e->getMessage();
+        } catch(PDOException $e) {
             session_unset();
             session_destroy();
-            echo <<<EOT
-                <!DOCTYPE html>
-                <html>
-                    <body>
-                        <script>alert("Internal Error.");window.location.replace("index.php");</script>
-                    </body>
-                </html>
-EOT;
+            sendPopup('Internal Error: ' . $e->getMessage(), 'index.php');
         }
+
+    }
+
+    function sendPopupAndGoto($msg, $page) {
+        echo <<<EOT
+            <!DOCTYPE html>
+            <html>
+                <body>
+                    <script>
+                        alert("$msg");
+                        window.location.replace("$page");
+                    </script>
+                </body>
+            </html>
+EOT;
     }
 ?>
