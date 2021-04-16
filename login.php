dd9a0ec0b7d66cec5b8aa02efc31a801e7017b4e
Alan-Kuan <kuanhaoan@gmail.com>
2021-04-16 10:58:29 +0800

    Add: backend validation.

--- login.php
+++ login.php
@@ -8,37 +8,40 @@
     $dbusername = getenv('MYSQL_USER');
     $dbpassword = getenv('MYSQL_PASSWORD');
 
-    if(isset($_POST['username']) && isset($_POST['password'])) {
+    if(!isset($_POST['username']) || !isset($_POST['password'])) {
+        session_unset();
+        session_destroy();
+        header('Location: index.php');
+        exit();
+    }
 
-        $username = $_POST['username'];
-        $hashed_password = hash('sha256', $_POST['password']);
+    $username = $_POST['username'];
+    $hashed_password = hash('sha256', $_POST['password']);
 
-        try {
+    try {
 
-            $conn = new PDO("mysql:host=$dbhostname;port=$dbport;dbname=$dbname", $dbusername, $dbpassword);
-            # set the PDO error mode to exception
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $stmt=$conn->prepare("SELECT password FROM users WHERE username = :username");
-            $stmt->execute(array('username' => $username));
+        $conn = new PDO("mysql:host=$dbhostname;port=$dbport;dbname=$dbname", $dbusername, $dbpassword);
+        # set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $stmt=$conn->prepare("SELECT password FROM users WHERE username = :username");
+        $stmt->execute(array('username' => $username));
 
-            if($stmt->rowCount() == 1 && $stmt->fetch()[0] === $hashed_password) {
-                $_SESSION['Authenticated'] = true;
-                $_SESSION['Username'] = $username;
-                header("Location: home.php");
-                exit();
-            } else {
-                session_unset();
-                session_destroy();
-                sendPopupAndGoto('登入失敗', 'index.php');
-                exit();
-            }
-
-        } catch(PDOException $e) {
+        if($stmt->rowCount() == 1 && $stmt->fetch()[0] === $hashed_password) {
+            $_SESSION['Authenticated'] = true;
+            $_SESSION['Username'] = $username;
+            header("Location: home.php");
+            exit();
+        } else {
             session_unset();
             session_destroy();
-            sendPopup('Internal Error: ' . $e->getMessage(), 'index.php');
+            sendPopupAndGoto('登入失敗', 'index.php');
+            exit();
         }
 
+    } catch(PDOException $e) {
+        session_unset();
+        session_destroy();
+        sendPopup('Internal Error: ' . $e->getMessage(), 'index.php');
     }
 
     function sendPopupAndGoto($msg, $page) {
--- register.php
+++ register.php
@@ -8,45 +8,56 @@
     $dbusername = getenv('MYSQL_USER');
     $dbpassword = getenv('MYSQL_PASSWORD');
 
-    if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['password-retype']) && isset($_POST['phone_num'])) {
+    if(!isset($_POST['username']) || !isset($_POST['password']) || !isset($_POST['password-retype']) || !isset($_POST['phone_num'])) {
+        session_unset();
+        session_destroy();
+        header('Location: index.php');
+        exit();
+    }
 
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        $password2 = $_POST['password-retype'];
-        $phone_num = $_POST['phone_num'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $password2 = $_POST['password-retype'];
+    $phone_num = $_POST['phone_num'];
 
-        if($password !== $password2) {
+    if(!validateUsername($username) || !validatePassword($password, $password2) || !validatePhonenum($phone_num)) {
+        session_unset();
+        session_destroy();
+        header('Location: index.php');
+        exit();
+    }
+
+    try {
+        $conn = new PDO("mysql:host=$dbhostname;port=$dbport;dbname=$dbname", $dbusername, $dbpassword);
+        # set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $stmt = $conn->prepare("SELECT username FROM users WHERE username = BINARY :username");
+        $stmt->execute(array('username' => $username));
+
+        // username already exists
+        if($stmt->rowCount() == 1) {
             session_unset();
             session_destroy();
+            header('Location: index.php');
+            exit();
+        } else {
+            $hashed_password = hash('sha256', $password);
+            $stmt = $conn->prepare("INSERT INTO users (username, password, phone_num) VALUES(:username, :password, :phone_num)");
+            $stmt->execute(array(
+                'username' => $username,
+                'password' => $hashed_password,
+                'phone_num' => $phone_num
+            ));
+            $_SESSION['Authenticated'] = true;
+            $_SESSION['Username'] = $username;
+            sendPopupAndGoto('註冊成功', 'home.php');
             exit();
         }
 
-        try {
-            $conn = new PDO("mysql:host=$dbhostname;port=$dbport;dbname=$dbname", $dbusername, $dbpassword);
-            # set the PDO error mode to exception
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $stmt = $conn->prepare("SELECT username FROM users WHERE username = BINARY :username");
-            $stmt->execute(array('username' => $username));
-
-            // username already exists
-            if($stmt->rowCount() == 1) {
-                session_unset();
-                session_destroy();
-                exit();
-            } else {
-                createAccount($username, $password, $phone_num);
-                $_SESSION['Authenticated'] = true;
-                $_SESSION['Username'] = $username;
-                sendPopupAndGoto('註冊成功', 'home.php');
-                exit();
-            }
-
-        } catch(PDOException $e) {
-            session_unset();
-            session_destroy();
-            sendPopupAndGoto('Internal Error: ' . $e->getMessage(), 'index.php');
-        }
-
+    } catch(PDOException $e) {
+        session_unset();
+        session_destroy();
+        sendPopupAndGoto('Internal Error: ' . $e->getMessage(), 'index.php');
     }
 
     function sendPopupAndGoto($msg, $page) {
@@ -63,32 +74,38 @@
 EOT;
     }
 
-    function createAccount($username, $password, $phone_num) {
+    function validateUsername($username) {
 
-        $hashed_password = hash('sha256', $password);
+        if(empty($username))
+            return false;
 
-        $dbhostname = getenv('MYSQL_HOST');
-        $dbport = '3306';
-        $dbname = getenv('MYSQL_DATABASE');
-        $dbusername = getenv('MYSQL_USER');
-        $dbpassword = getenv('MYSQL_PASSWORD');
+        return preg_match('/^[0-9\.a-zA-Z]+$/', $username);
 
-        try {
-            $conn = new PDO("mysql:host=$dbhostname;port=$dbport;dbname=$dbname", $dbusername, $dbpassword);
-            # set the PDO error mode to exception
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $stmt = $conn->prepare("INSERT INTO users (username, password, phone_num) VALUES(:username, :password, :phone_num)");
-            $stmt->execute(array(
-                'username' => $username,
-                'password' => $hashed_password,
-                'phone_num' => $phone_num
-            ));
+    }
 
-        } catch(PDOException $e) {
-            session_unset();
-            session_destroy();
-            sendPopupAndGoto('Internal Error: ' . $e->getMessage(), 'index.php');
-        }
+    function validatePassword($password, $password2) {
+
+        if(empty($password) || empty($password2))
+            return false;
+        if($password !== $password2)
+            return false;
+        if(strlen($password) < 8)
+            return false;
+
+        return preg_match('/^[\ -~]+$/', $password);
+
+    }
+
+    function validatePhonenum($phone_num) {
+
+        if(empty($phone_num))
+            return false;
+
+        $len = strlen($phone_num);
+        if($len < 7 || $len > 10)
+            return false;
+
+        return preg_match('/^[0-9]+$/', $phone_num);
 
     }
 ?>
