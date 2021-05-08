6fa36ce37d742ccf96ac7be0a82df79236da863b
Alan-Kuan <kuanhaoan@gmail.com>
2021-05-08 10:34:23 +0800

    Change: remove redundant space and newline.

--- components/amount-select.php
+++ components/amount-select.php
@@ -1,11 +1,11 @@
 <select class="form-select" aria-label="amount select" name="amount_range">
-    <option value='out-of-stock' <?= isset($_GET['amount_range']) && ($_GET['amount_range'] === 'out-of-stock' ) ? 'selected' : ''; ?> >
+    <option value='out-of-stock' <?= isset($_GET['amount_range']) && ($_GET['amount_range'] === 'out-of-stock') ? 'selected' : ''; ?>>
         <?= $TEXT['out-of-stock']; ?>
     </option>
-    <option value='few' <?= isset($_GET['amount_range']) && ($_GET['amount_range'] === 'few' ) ? 'selected' : ''; ?>>
+    <option value='few' <?= isset($_GET['amount_range']) && ($_GET['amount_range'] === 'few') ? 'selected' : ''; ?>>
         <?= $TEXT['few']; ?>
     </option>
-    <option value='sufficient' <?= isset($_GET['amount_range']) && ($_GET['amount_range'] === 'sufficient' ) ? 'selected' : ''; ?>>
+    <option value='sufficient' <?= isset($_GET['amount_range']) && ($_GET['amount_range'] === 'sufficient') ? 'selected' : ''; ?>>
         <?= $TEXT['sufficient']; ?>
     </option>
 </select>
